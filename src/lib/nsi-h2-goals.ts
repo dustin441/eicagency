e6cb1728fc75f5c3bef9963cf2403450991feafd
root <root@srv1690776.hstgr.cloud>
2026-08-05 17:36:39 +0000

    Lower NSI H2 CPS goal and remove goal banner

--- src/lib/nsi-h2-goals.ts
+++ src/lib/nsi-h2-goals.ts
@@ -1,11 +1,11 @@
-export const NSI_H2_PLAN_URL = 'https://docs.google.com/spreadsheets/d/1LeiqgzXYvj68Th4FKIYT6h26fiuOBYVz/edit?gid=1280102363#gid=1280102363';
+export const NSI_H2_PLAN_URL = 'https://docs.google.com/spreadsheets/d/1LeiqgzXYvj68Th4FKIYT6h26fiuOBYVz/edit?gid=1362323845#gid=1362323845';
 
 export const NSI_H2_GOALS = {
   mediaBudget: 104_000,
   engagedSessions: 55_703,
   costPerEngagedSession: 1.87,
-  submittals: 610,
-  costPerSubmittal: 170.49,
+  submittals: 670,
+  costPerSubmittal: 155,
   coreCampaignCostPerSubmittalGuardrail: 155,
   submittalRate: 0.005,
   directResponseShare: 0.8,
